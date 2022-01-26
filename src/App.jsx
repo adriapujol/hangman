1cd93d812cffd0ce0c95a10f93c8bea504ae5b50
Adria Pujol <adriapujolaparicio@gmail.com>
2022-01-26 19:59:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <nav>
+
+      </nav>
+      <section className="">
+
+      </section>
+      <section>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
