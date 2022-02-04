ab1df020831740fdad912fcc1d89d895f2223819
Adria Pujol <adriapujolaparicio@gmail.com>
2022-02-04 12:59:22 +0100

    background test

--- src/App.jsx
+++ src/App.jsx
@@ -95,19 +95,6 @@ function App() {
         </div>
       </nav>
       <section className="game">
-        {/* <img src={background} alt="Background" /> */}
-        {/* <img src={background} alt="Background" />
-        <img src={woodL} alt="Structure" className={lives < 10 ? "structure st1" : "structure st1 hide"} />
-        <img src={woodM} alt="Structure" className={lives < 9 ? "structure st2" : "structure st2 hide"} />
-        <img src={woodS} alt="Structure" className={lives < 8 ? "structure st3" : "structure st3 hide"} />
-        <img src={body} alt="Body" className={lives < 5 ? "man body" : "man body hide"} />
-        <img src={rope} alt="Rope" className={lives < 7 ? "rope" : "rope hide"} />
-        <img src={head} alt="Head" className={lives < 6 ? "man head" : "man head hide"} />
-        <img src={armLeft} alt="Left arm" className={lives < 4 ? "man limbs arm1" : "man limbs arm1 hide"} />
-        <img src={armRight} alt="Right arm" className={lives < 3 ? "man limbs arm2" : "man limbs arm2 hide"} />
-        <img src={legLeft} alt="Left leg" className={lives < 2 ? "man limbs leg1" : "man limbs leg1 hide"} />
-        <img src={legRight} alt="Right leg" className={lives < 1 ? "man limbs leg2" : "man limbs leg2 hide"} /> */}
-
         <div className="word">
           {
             letters.map((letter, index) => {
@@ -132,30 +119,6 @@ function App() {
           <div className={lives < 2 ? "hm legLeft" : "hm legLeft hide"}></div>
           <div className={lives < 1 ? "hm legRight" : "hm legRight hide"}></div>
         </div>
-        {/* <div className="hangman"> */}
-
-        {/* <img src={background} alt="Background" />
-          <img src={woodL} alt="Structure" />
-          <img src={woodM} alt="Structure" />
-          <img src={woodS} alt="Structure" />
-          <img src={rope} alt="Rope" />
-          <img src={head} alt="Head" />
-          <img src={body} alt="Body" />
-          <img src={armLeft} alt="Left arm" />
-          <img src={armRight} alt="Right arm" />
-          <img src={legLeft} alt="Left leg" />
-          <img src={legRight} alt="Right leg" /> */}
-        {/* <div className={lives < 10 ? "structure st1" : "structure st1 hide"}></div>
-          <div className={lives < 9 ? "structure st2" : "structure st2 hide"}></div>
-          <div className={lives < 8 ? "structure st3" : "structure st3 hide"}></div>
-          <div className={lives < 7 ? "rope" : "rope hide"}></div>
-          <div className={lives < 6 ? "man head" : "man head hide"}></div>
-          <div className={lives < 5 ? "man body" : "man body hide"}></div>
-          <div className={lives < 4 ? "man limbs arm1" : "man limbs arm1 hide"}></div>
-          <div className={lives < 3 ? "man limbs arm2" : "man limbs arm2 hide"}></div>
-          <div className={lives < 2 ? "man limbs leg1" : "man limbs leg1 hide"}></div>
-          <div className={lives < 1 ? "man limbs leg2" : "man limbs leg2 hide"}></div> */}
-        {/* </div> */}
       </section>
       <Keyboard checkLetter={checkLetter} correctGuesses={correctGuesses} wrongGuesses={wrongGuesses} />
     </div>
