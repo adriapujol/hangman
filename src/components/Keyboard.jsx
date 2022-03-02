a40033e0cf06366a14fc945555b273688249b91c
Adria Pujol <adriapujolaparicio@gmail.com>
2022-03-02 17:20:28 +0100

    no enter key

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -31,7 +31,7 @@ function Keyboard({ checkLetter, correctGuesses, wrongGuesses }) {
 
     const keys1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
     const keys2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
-    const keys3 = ["solve", "z", "x", "c", "v", "b", "n", "m", "enter"];
+    const keys3 = ["solve", "z", "x", "c", "v", "b", "n", "m"];
 
     const guesses = [...correctGuesses, ...wrongGuesses];
 
